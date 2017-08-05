00eaa74bd26a73f469747b7e67650e61f2a7875d
Iurii Rodyk <i.rodyk@AMDC2909.digital.local>
2017-08-05 10:33:17 +0200

    initial commit

---
+++ src/main/java/com/irodyk/giflib/controller/GifController.java
@@ -0,0 +1,15 @@
+package com.irodyk.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GifController {
+
+
+    @RequestMapping("/")
+    public String listGifts(){
+        return "home";
+    }
+
+}
